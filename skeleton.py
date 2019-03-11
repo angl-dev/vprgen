ea7c8759c5de44016d6171492aeb847210d95539
Ang Li <leon575777642@gmail.com>
2019-03-10 22:28:42 -0400

    Segment, layout

---
+++ skeleton.py
@@ -0,0 +1,55 @@
+class ArchitectureDelegate(object):
+    """Delegate class which is able to answer questions about what are in the architecture."""
+
+    def iter_models(self):
+        """Iterate or generate data for the <model> tags under the <models> tag in VPR's architecture description XML.
+
+        Each element in the returned iterator/generator should be a `dict` satisfying the JSON schema
+        'schema/model.schema.json'.
+        """
+        return
+        yield None  # mark this method as a generator
+
+    def iter_blocks(self):
+        """Iterate or generate blocks for the <pb_type> tags under the <complexblocklist> tag in VPR's architecture
+        description XML and the <block_type> tags under the <block_types> tag in VPR's routing resource graph XML.
+
+        Each element in the returned iterator/generator should be a `dict` satisfying the JSON schema
+        'schema/block.schema.json'. The order of the ports will be used to determine the `pin_id` of each port.
+        """
+        return
+        yield None  # mark this method as a generator
+
+    def width(self):
+        """Width of the FPGA grid."""
+        raise NotImplementedError
+
+    def height(self):
+        """Height of the FPGA grid."""
+        raise NotImplementedError
+
+    def get_tile(self, x, y):
+        """Get the physical block at tile (x, y).
+
+        The returned value should be None or a `dict` satisfying the JSON schema 'schema/tile.schema.json'. If None is
+        returned, the tile is treated as "EMPTY".
+        """
+        return None
+
+    def get_device(self):
+        """Get the device information.
+
+        The returned value should be a `dict` satisfying the JSON schema 'schema/device.schema.json'. If None is
+        returned, a fake <device> tag will be generated and used.
+        """
+        return None
+
+    def iter_segments(self):
+        """Iterate or generate segments for the <segment> tags under the <segmentlist> tag in VPR's architecture
+        description XML and the <segment> tag under the <segments> tag in VPR's routing resource graph XML.
+
+        Each element in the returned iterator/generator should be a `dict` satisfying the JSON schema
+        'schema/segment.schema.json'.
+        """
+        return
+        yield None  # mark this method as a generator
